6f2d5590ee53d76d2ee6d26b6ac522574b1a3611
Obijuan <obijuan.cube@gmail.com>
2023-07-10 09:19:15 +0200

    Pong: add image

--- 02-ZX-Pong/Paso-01/Main.asm
+++ 02-ZX-Pong/Paso-01/Main.asm
@@ -1,6 +1,7 @@
 
     org $8000
 
+    ;-- Dibujar Linea vertical izquierda
     ;-- Apuntar a la celda (0,0)
     ld hl, $4000
     ld b, 192  ;-- Recorrer las 192 scanlines...
@@ -10,9 +11,23 @@ loop:
 
     ;-- DEBUG: Hacer una pausa para ver como se
     ;-- dibuja la linea de arriba hacia abajo
-    halt  
+    ;halt  
     djnz loop
 
+    ;--------------- Dibujar linea vertical derecha
+    ;-- La dibujamos de abajo hacia arriba, para probar la
+    ;-- rutina PreviousScan
+    ld hl, $57FF  ;-- Posicion: (255,192)
+    ld b,192      ;-- Recorrer las 192 scanlines
+
+loop2:
+    ld (hl), $3c
+    call PreviousScan
+
+    ;-- DEBUG: Hacer una pausa para ver el dibujo
+    ;-- de la linea
+    ;-- halt
+    djnz loop2
     ret
 
     include "Video.asm"
--- 02-ZX-Pong/Paso-01/Video.asm
+++ 02-ZX-Pong/Paso-01/Video.asm
@@ -29,3 +29,35 @@ NextScan:
     ld h, a
     ret
     
+;--------------------------------------------------------------------
+;--  PreviousScan: Obtener la direccion scanline anterior
+;--
+;--  ENTRADA:
+;--    * HL: Scanline actual
+;--  SALIDA:
+;--    * HL: Scanline anterior
+;--
+;--  Formato direccion: 010TTSSS LLLCCCCC
+;---------------------------------------------------------------------
+PreviousScan:
+
+    ;-- Decrementar el scanline
+    ld a,h
+    dec h
+    and $7
+    ret nz  ;-- Nos quedamos en la fila actual
+
+    ;-- El scanline está en la fila anterior. Hay que decrementar
+    ;-- la fila
+    ld a, l
+    sub $20
+    ld l,a
+    ret c  ;-- Hemos cambiado de fila, y de tercio
+
+    ;-- Hay que incrementar el tercio para que se quede como está
+    ld a,h 
+    add a, $08
+    ld h,a
+
+    ret
+
